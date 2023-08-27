1837bc9378f0930d86305b7c9dac6fe5836bd178
Tarik_B <tarik_b@MBP-de-Loubna.lan>
2023-08-27 22:52:05 +0200

    👮setting up login form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,13 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Ingredient;
+use Faker\Factory;
+use App\Entity\User;
+use Faker\Generator;
 use App\Entity\Recipe;
+use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Faker\Factory;
-use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
@@ -15,6 +16,7 @@ class AppFixtures extends Fixture
      * @var Generator
      */
     private Generator $faker;
+
 
     public function __construct()
     {
@@ -51,6 +53,20 @@ class AppFixtures extends Fixture
 
             $manager->persist($recipe);
         }
+        //Users
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User;
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
+
+            $manager->persist($user);
+        }
+
+
+
         $manager->flush();
     }
 }
